2ad73bf39107be9c2ea4297f81526b5f7dcf4698
Dmitriy Kurenkov <demel654@gmail.com>
2022-06-01 19:49:09 +0300

    before eject

--- app/screens/main/components/decks.tsx
+++ app/screens/main/components/decks.tsx
@@ -1,10 +1,16 @@
 import { StackNavigationProp } from "@react-navigation/stack"
-import { StorageAccessFramework } from "expo-file-system"
+import {
+  documentDirectory,
+  getContentUriAsync,
+  getInfoAsync,
+  StorageAccessFramework,
+} from "expo-file-system"
 import { FlatList, View, ViewStyle } from "react-native"
 import { useStores } from "../../../models"
 import { Deck } from "../../../models/deck/deck"
 import { NavigatorParamList } from "../../../navigators"
 import * as FileSystem from "expo-file-system"
+import * as Sharing from "expo-sharing"
 
 import {
   Menu,
@@ -19,6 +25,8 @@ import { observer } from "mobx-react-lite"
 import * as MediaLibrary from "expo-media-library"
 import React, { FC, useCallback, useEffect, useState } from "react"
 import { ThemeContext } from "../../../app"
+
+import { zip } from "react-native-zip-archive"
 
 const FULL: ViewStyle = { flex: 1 }
 
@@ -121,8 +129,13 @@ export const Decks = withMenuContext<DeckProps & MenuContextProps>(
       // const asset = await MediaLibrary.createAssetAsync(
       //   "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540concat17%252FMemoSurf/ImagePicker/e2f608ac-61f8-46fc-8c1c-bd3adcefebc5.jpg",
       // )
+
       // await MediaLibrary.createAlbumAsync("KekLol", asset, false)
 
+      const perm = await MediaLibrary.requestPermissionsAsync(false)
+      if (!perm.granted) {
+        return
+      }
       const album = await MediaLibrary.getAlbumAsync("KekLol")
       console.log("album", album)
       console.log("loc", album.locationNames)
@@ -131,33 +144,43 @@ export const Decks = withMenuContext<DeckProps & MenuContextProps>(
       // if (assets.length) {
       //   await MediaLibrary.addAssetsToAlbumAsync(assets, newAlbum, false);
       // }
+      // const source = `file:///storage/emulated/0/Pictures/KekLol/e2f608ac-61f8-46fc-8c1c-bd3adcefebc5.jpg`
+      const source = assets.assets[0].uri
+      console.log("sor", source)
+      const assetInfo = await MediaLibrary.getAssetInfoAsync(assets.assets[0])
+      console.log("assets", assetInfo)
+
+      const dirPath = documentDirectory + "/packs"
+      const dirInfo = await getInfoAsync(dirPath)
+      if (!dirInfo.exists) {
+        await FileSystem.makeDirectoryAsync(dirPath)
+      }
+
+      const contentSource = await getContentUriAsync(dirPath)
+
+      console.log("con", contentSource)
+      // await Sharing.shareAsync(dirPath)
+      zip(dirPath, dirPath)
     }
 
     const createFolderExternal = async (deck: Deck) => {
-      const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync()
-
-      if (!permissions.granted) {
-        return
-      }
-
-      const grantedPath = permissions.directoryUri
-      console.log("permissions granted", grantedPath)
-
-      const path = await StorageAccessFramework.makeDirectoryAsync(grantedPath, deck.name)
-      console.log("folder created", path)
-
-      const preparedDeck = {
-        ...deck,
-        cards: deck.cards.map((c) => ({ ...c, interval: 0, eFactor: 0, repetitions: 0 })),
-      }
-
-      const uri = await StorageAccessFramework.createFileAsync(
-        path,
-        deck.name + ".json",
-        "application/json",
-      )
-
-      await FileSystem.writeAsStringAsync(uri, JSON.stringify(preparedDeck))
+      MediaLibrary.requestPermissionsAsync(false)
+        .then((permissionRes) => {
+          if (permissionRes.granted) {
+            MediaLibrary.getAlbumsAsync({ includeSmartAlbums: true })
+              .then((res) => {
+                console.log("Successs : ", res)
+              })
+              .catch((err) => {
+                console.log("Error : ", err.message)
+              })
+          } else {
+            console.log("Something went wrong.")
+          }
+        })
+        .catch((err) => {
+          console.log("Error : ", err.message)
+        })
     }
     const goDeckParamsScreen = useCallback(
       (deck: Deck) => nav.navigate("deckParamsEditor", { deck }),
@@ -188,7 +211,7 @@ export const Decks = withMenuContext<DeckProps & MenuContextProps>(
             <MenuOption onSelect={() => goDeckParamsScreen(selectedDeck)} text="Edit" />
             {/* <MenuOption onSelect={() => exportDeck(selectedDeck)} text="Export" /> */}
             <MenuOption onSelect={() => collection.deleteDeck(selectedDeck)} text="Delete" />
-            {/* <MenuOption onSelect={() => createFolder(selectedDeck)} text="test" /> */}
+            <MenuOption onSelect={() => createFolder(selectedDeck)} text="test" />
           </MenuOptions>
         </Menu>
         <FlatList
